b7a0f7ff9b69587d907ca2f51910f298a86be13c
RubenErhardt <rubengamebattles@outlook.com>
2024-06-03 20:09:35 +0200

    Node opzet
    - NPM install
    - Public folder
    - Views folder
    - Partials
    - Server.js opgezet
    - fetch-json.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('Hier komt je server voor Sprint 12.')
+import express from 'express'
+import fetchJson from './helpers/fetch-json.js'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.set('port', process.env.PORT || 8009)
+
+app.listen(app.get('port'), function () {
+ console.log(`Application started on http://localhost:${app.get('port')}`)
+})
+
+app.get('/', function (request, response) {
+    response.render('index', {
+    })
+})
