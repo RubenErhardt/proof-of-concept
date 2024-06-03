8255a81ca918bf1c946f52bce3e44ccc758edce7
RubenErhardt <rubengamebattles@outlook.com>
2024-06-03 20:38:26 +0200

    API DATA
    - item id
    - item title
    - item image

--- server.js
+++ server.js
@@ -9,11 +9,23 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.set('port', process.env.PORT || 8009)
 
-app.listen(app.get('port'), function () {
- console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+const apiUrl = 'https://fdnd-agency.directus.app/items/fabrique_art_objects'
+
+// Data ophalen bij opstart en filteren
+const data = await fetchJson(apiUrl)
+const filteredData = data.data.map(item => ({
+    id: item.id,
+    title: item.title,
+    image: item.image
+}))
 
 app.get('/', function (request, response) {
     response.render('index', {
+        data: filteredData
     })
+    console.log(filteredData)   
+})
+
+app.listen(app.get('port'), function () {
+    console.log(`Application started on http://localhost:${app.get('port')}`)
 })
