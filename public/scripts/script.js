9f63e8a66bfc125943139d1110f37787f5f266a7
RubenErhardt <rubengamebattles@outlook.com>
2024-06-12 11:14:22 +0200

    onscroll beter

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,13 +1,22 @@
-function loadMoreItems() {
-    const list = document.getElementById('art-objects-list');
-    
-    const items = list.querySelectorAll('li');
-    items.forEach(item => {
-        const clone = item.cloneNode(true);
-        list.appendChild(clone);
-    });
-  }
-  
+  	    const maxItems = 300; 
+        const itemsToClone = 5;
+        let totalClonedItems = 0;
+
+        function loadMoreItems() {
+            const list = document.getElementById('art-objects-list');
+            const items = list.querySelectorAll('li');
+            const itemsCount = items.length;
+
+            if (totalClonedItems + itemsToClone > maxItems) return; 
+
+            for (let i = 0; i < itemsToClone; i++) {
+                const item = items[i % itemsCount]; 
+                const clone = item.cloneNode(true);
+                list.appendChild(clone);
+                totalClonedItems++;
+            }
+        }
+
   function handleScroll() {
     const scrollableWidth = document.documentElement.scrollWidth;
     const currentScroll = window.scrollX + window.innerWidth;
